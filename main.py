1b724756ca960661116d7f7a85fe5ee483d55d96
Rasmus Englund <appdev@redge.se>
2021-02-09 23:56:49 +0100

    Add combining main file

---
+++ main.py
@@ -0,0 +1,16 @@
+from link_scraper import get_links
+from image_scraper import scrape
+
+
+if __name__ == "__main__":
+
+    BLOCKED_IMAGES = [
+        "http://www.stjorgengk.com/Images/logo.png"
+    ]
+
+    PATH = r"/Users/RasmusEnglund/Desktop/Scraper/result"
+    bURL = "http://www.stjorgengk.com"
+
+    links, _ = get_links(bURL)
+
+    print(len(links))
