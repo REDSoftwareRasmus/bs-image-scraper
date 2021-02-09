b8a3986778123b16720dc6875bb990fea4f55b88
Rasmus Englund <appdev@redge.se>
2021-02-09 23:40:26 +0100

    Add cssutils

--- link_scraper.py
+++ link_scraper.py
@@ -1,33 +1,4 @@
 import requests
-import os
-from tqdm import tqdm
-from bs4 import BeautifulSoup as bs
-from urllib.parse import urljoin, urlparse
-
-
-def is_valid(url):
-    """
-    Checks whether `url` is a valid URL.
-    """
-    parsed = urlparse(url)
-    return bool(parsed.netloc) and bool(parsed.scheme)
-
-
-if __name__ == "__main__":
-
-    BLOCKED = [
-        "http://www.stjorgengk.com/Images/logo.png"
-    ]
-
-    URLs = [
-        "http://www.stjorgengk.com",
-        "http://www.stjorgengk.com/banan"
-    ]
-
-    PATH = r"/Users/RasmusEnglund/Desktop/Scraper/result"
-
-    scrape(URLs, PATH)
-
-
-
-
+from urllib.parse import urlparse, urljoin
+from bs4 import BeautifulSoup
+import colorama
